fa1baf9e8c9e90652656e83bb6c524e03490d491
Hamza960 <53042077+Hamza960@users.noreply.github.com>
2021-12-12 21:22:18 +0530

    Add files via upload
    done

---
+++ footer.php
@@ -0,0 +1,39 @@
+<footer class="footer">
+                <div class="p-5">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-4 text-center">
+                                <h3><i class="fa fa-map-marker-alt"></i></h3>
+                                <p>Silver Estates Corporation, Shop No. 5, OYO Town House, Taboot Street Camp, Pune.</p>
+                            </div>
+                            <div class="col-md-4 text-center">
+                                <h4><i class="fa fa-envelope"></i></h4>
+                                <p>Saleem Shaikh</p>
+                                <p>info@silverestatescorporation.com</p>
+
+
+                            </div>
+                            <div class="col-md-4">
+                                <div class="footer_card">
+                                    <div class="footer-card-inner">
+                                        <p><i class="fa fa-phone"></i>&nbsp;+91 9890851007</p>
+
+                                        <p><i class="fa fa-map-marker-alt"></i>&nbsp;Office No. 3, "Surya Suman", near Neptune Soc., Kalyani Nagar, Pune</p>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                        <hr>
+                    </div>
+                </div>
+            </footer>
+
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" referrerpolicy="no-referrer">
+</script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
+</script>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
+</script>
+</body>
---
+++ index.php
@@ -0,0 +1,178 @@
+    <?php include('header.php') ?>
+    
+    <div class="banner pb-5">
+        <div class="container">
+            <div class="row ">
+                <div class="col-md-5">
+                    <div class="mt-5  banner_img">
+                        <img src="images/logos/silver-estate-logo.png" class="img-fluid" width="350">
+                    </div>
+                </div>
+                <div class="col-md-7">
+                    <div class="mt-5  pl-5">
+                        <h1 class="main_title">SILVER ESTATES CORPORATION</h1>
+                        <p class="main_desc">Everything In Realty Sector With Excellence!</p>
+                        <ul class="main_list">
+                            <li>Promoters & Builders</li>
+                            <li>Government Licensed Engineers & Contractors</li>
+                            <li>Project Managememnt Consultant</li>
+                            <li>Real Estate Consultants</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div class="objective p-3">
+            <p class="mb-0">Our objective is to fulfill the espectations of our clients and provide with the best value for money.</p>
+        </div>
+
+        <div class="container">
+            <div class="row button_section">
+                <div class="col-md-4 text-center">
+                    <button class="service_button">India</button>
+                </div>
+                <div class="col-md-4 text-center">
+                    <button class="service_button">U.A.E</button>
+                </div>
+                <div class="col-md-4 text-center">
+                    <button class="service_button">K.S.A</button>
+                </div>
+            </div>
+        </div>
+    </div>   
+            <div class="about_us_section">
+                <div class="container">
+                    <div class="text-center">
+                        <h2>About Us</h2>
+                    </div>
+                    <div class = "row">
+                        <div class = "col-md-12">
+                            <h3>We are a group of companies into following OPERATIONS...</h3>
+                        </div>
+                    </div>
+                    <div class = "row pb-5">
+                        <div class = "col-md-6">
+                            <div class = "about_silver">
+                                <h4>
+                                Silver Estates Corporation (India)
+                                </h4>
+                                <ul>
+                                    <li>Promoters and Builders</li>
+                                    <li>Government Licensed engineers & Contractors</li>
+                                    <li>Project Management consultants</li>
+                                    <li>Real estate consultants, Legal & Liasoning services</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class = "col-md-6">
+                            <div class = "about_red">
+                                <h4>Red Design Technical Work LLC (U.A.E.)</h4>
+                                <ul>
+                                    <li>Architectural and Interior design, super vision and execution i.e., concept to 
+                                        completion for turnkey projects</li>
+                                   
+                                </ul>
+                            </div>
+                            <div class = "about_red_india">
+                                <h4>Red Realty (India)</h4>
+                                <ul>
+                                    <li>Dealing in properties, Trading in propertie</li>
+                                   
+                                </ul>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class = "our_mission_section">
+            <div class="container">
+  <h1 class="center">Our Mission</h1>
+  
+  
+  
+  <article class="episode mt-5">
+    <div class="episode__number">01</div>
+    <div class="episode__content">
+      <div class="title">Quality</div>
+      <div class="story">
+        <p>Our mission is to provide a high quality services in Realty sector</p>
+        
+        
+      </div>
+    </div>
+  </article>
+  
+  <article class="episode">
+    <div class="episode__number">02</div>
+    <div class="episode__content">
+      <div class="title">Punctuality</div>
+      <div class="story">
+        <p>Projects will be delivered on time and on budget with paying attention to 
+details.</p>
+       
+      </div>
+    </div>
+  </article>
+  
+  <article class="episode">
+    <div class="episode__number">03</div>
+    <div class="episode__content">
+      <div class="title">Passion</div>
+      <div class="story">
+        <p>We work with passion and we deliver the best service experience.</p>
+        
+      </div>
+    </div>
+  </article>
+  <article class="episode">
+    <div class="episode__number">04</div>
+    <div class="episode__content">
+      <div class="title">Excellence</div>
+      <div class="story">
+        <p>We apply the highest standards of excellence in design.</p>
+        
+      </div>
+    </div>
+  </article>
+  <article class="episode">
+    <div class="episode__number">05</div>
+    <div class="episode__content">
+      <div class="title">Limitless</div>
+      <div class="story">
+        <p>Always exceed our client’s expectations.</p>
+        
+      </div>
+    </div>
+  </article>
+  <article class="episode">
+    <div class="episode__number">06</div>
+    <div class="episode__content">
+      <div class="title">Influence</div>
+      <div class="story">
+        <p>To influence the lives of our clients and leave ever-lasting experience in their 
+mind. Our designs must inspire all those whoever lay eye on it.</p>
+        
+      </div>
+    </div>
+  </article>
+  <article class="episode">
+    <div class="episode__number">07</div>
+    <div class="episode__content">
+      <div class="title">Service</div>
+      <div class="story">
+        <p>We go one step further-to offer an exclusive customer services.</p>
+        
+      </div>
+    </div>
+  </article>
+</div>
+            </div>
+
+            <?php include('footer.php') ?>
+
+
+
+
+</html>
